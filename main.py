9fcf5cafbe4653c6fdbd2dcb215f87c165b3da06
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-11-05 03:52:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def salary(role, average):
+  print("For a/an", role)
+  print("The average income is ", average)
+salary("Analyst", 1250000 )
+salary("Pentester", 120000)
+
+hours=(35)
+average=(200000)
+income=(hours*average)
+print("Your income this month is ", income)
