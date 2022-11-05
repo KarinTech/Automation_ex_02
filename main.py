e28225cdbdb41a6f70815bebba4a84a13d66456a
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-11-05 01:16:19 -0300

    Update main.py
    added an area calculator -for later use in a construction calculate programming.

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ hours=(35)
 average=(200000)
 income=(hours*average)
 print("Your income this month is ", income)
+
+#areas-calculator
+def area(base, height):
+   return base*height
+area_1=area(2,3)
+area_2=area(5,4)
+full_area=area_1+area_2
+print("The full area of the first field is ", full_area)
+
+area_2=area(8,5)
+area_3=area(20,4)
+field_2=area_2+area_3
+print("The full area of the  second field is ", field_2)
